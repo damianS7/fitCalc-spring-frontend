c88800d89150759c4ea9eee755f215f465a24787
damian <git2507>
2021-07-14 00:02:43 +0200

    ...

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# hello-world
+# Fitness Kcalc
 
 ## Project setup
 ```
--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue } from 'bootstrap-vue'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
@@ -7,8 +7,6 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -17,8 +15,8 @@ library.add(fas)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 // My app
-import router from './router/index'
-import store from './store/index'
+import router from './router'
+import store from './store'
 import App from './App.vue'
 
 Vue.config.productionTip = false
