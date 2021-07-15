f3c10a00a7d9de1dcf5eae17a0317aa2fc1e5562
damian <git2507>
2021-07-16 01:04:35 +0200

    Necesario para que vue procese  el atributo src de componentes b-img etc ...

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
+const options = {
+  transformAssetUrls: {
+    video: ['src', 'poster'],
+    source: 'src',
+    img: 'src',
+    image: 'xlink:href',
+    'b-avatar': 'src',
+    'b-img': 'src',
+    'b-img-lazy': ['src', 'blank-src'],
+    'b-card': 'img-src',
+    'b-card-img': 'src',
+    'b-card-img-lazy': ['src', 'blank-src'],
+    'b-carousel-slide': 'img-src',
+    'b-embed': 'src'
+  }
+}
+
 module.exports = {
   // Modifica las rutas usadas por npm build
   // publicPath: './',
@@ -5,5 +22,25 @@ module.exports = {
     index: {
       entry: 'src/app.js'
     }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options["transformAssetUrls"] = {
+          'img': 'src',
+          'image': 'xlink:href',
+          'b-img': 'src',
+          'b-img-lazy': ['src', 'blank-src'],
+          'b-card': 'img-src',
+          'b-card-img': 'img-src',
+          'b-carousel-slide': 'img-src',
+          'b-embed': 'src'
+        };
+
+        return options;
+      });
   }
 }
