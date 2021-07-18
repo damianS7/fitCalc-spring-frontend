90b4e32db8ae69e39a3da2256e3bd62fe662e685
damian <git2507>
2021-07-18 23:12:43 +0200

    El dashboard ahora muestra los componentes de profile y weight

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,35 @@
 <template>
   <b-col cols="12">
+    <b-row>
+      <b-col>
+        <profile-summary></profile-summary>
+      </b-col>
+    </b-row>
+
+    <b-row>
+      <b-col>
+        <weight-chart :chartData="chartData"></weight-chart>
+      </b-col>
+    </b-row>
     <p>Calorias consumidas, restantes y objetivo</p>
-    <p>Graficos</p>
   </b-col>
 </template>
 
 <script>
-// @ is an alias to /src
-//import HelloWorld from "@/components/HelloWorld.vue";
-
+import ProfileSummary from "@/components/ProfileSummary.vue";
+import WeightChart from "@/components/WeightChart.vue";
+import { mapGetters } from "vuex";
+const components = {
+  "profile-summary": ProfileSummary,
+  "weight-chart": WeightChart,
+};
+const computed = {
+  ...mapGetters({
+    chartData: "getChartDataWeights",
+  }),
+};
 export default {
-  name: "Dashboard",
+  components,
+  computed,
 };
 </script>
