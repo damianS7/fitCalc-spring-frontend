d0c5e7374a45bd78d429ccbbf6ce6fdad99fdd86
damian <git2507>
2021-07-18 16:28:03 +0200

    Bootstrap 4 cambios

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,84 +1,70 @@
 <template>
   <b-col cols="12" class="profile">
+    <profile-summary></profile-summary>
+    <weight-chart></weight-chart>
+
     <b-row>
-      <profile-summary></profile-summary>
+      <b-col>
+        <b-form>
+          <b-form-group label="Tu altura:" description="En centimetros.">
+            <b-form-input
+              id="height"
+              type="text"
+              v-model="height"
+              placeholder="Introduce tu altura"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group label="Tu nombre:">
+            <b-form-input
+              id="username"
+              type="text"
+              v-model="username"
+              placeholder="Introduce tu nombre de usuario"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group label="Tu email:">
+            <b-form-input
+              id="email"
+              type="email"
+              v-model="email"
+              placeholder="Introduce tu correo electronico"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group label="Cambiar password:">
+            <b-form-input
+              id="pass1"
+              type="password"
+              v-model="password"
+              placeholder="Introduce tu nueva password"
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group label="Repite la password:">
+            <b-form-input
+              id="pass2"
+              type="password"
+              v-model="repeatedPassword"
+              placeholder="Repite tu nueva password"
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group label=" ">
+            <b-button type="submit" variant="primary" class="w-100"
+              >Actualizar</b-button
+            >
+          </b-form-group>
+        </b-form>
+      </b-col>
     </b-row>
-    <b-form>
-      <b-form-group label="Tu altura:" description="En centimetros.">
-        <b-form-input
-          id="height"
-          type="text"
-          v-model="height"
-          placeholder="Introduce tu altura"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group label="Tu peso:" description="En kilogramos.">
-        <b-form-input
-          id="weight"
-          type="text"
-          v-model="weight"
-          placeholder="Introduce tu peso"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group label="Tu nombre:">
-        <b-form-input
-          id="username"
-          type="text"
-          v-model="username"
-          placeholder="Introduce tu nombre de usuario"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group label="Tu email:">
-        <b-form-input
-          id="email"
-          type="email"
-          v-model="email"
-          placeholder="Introduce tu correo electronico"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group label="Cambiar password:">
-        <b-form-input
-          id="pass1"
-          type="password"
-          v-model="password"
-          placeholder="Introduce tu nueva password"
-        ></b-form-input>
-      </b-form-group>
-      <b-form-group label="Repite la password:">
-        <b-form-input
-          id="pass2"
-          type="password"
-          v-model="repeatedPassword"
-          placeholder="Repite tu nueva password"
-        ></b-form-input>
-      </b-form-group>
-      <b-form-group label=" ">
-        <b-button type="submit" variant="primary" class="w-100"
-          >Actualizar</b-button
-        >
-      </b-form-group>
-    </b-form>
   </b-col>
 </template>
 
 <script>
 import ProfileSummary from "@/views/profile/ProfileSummary.vue";
+import Weight from "@/views/profile/Weight.vue";
 import { mapGetters, mapState } from "vuex";
-const data = function () {
-  return {
-    username: "this.user.username",
-    email: "this.user.email",
-    password: "******",
-    repeatedPassword: "",
-    age: 33,
-    height: 177,
-    weight: 21,
-  };
-};
+
 const computed = {
   ...mapState({
     user: "user",
@@ -89,8 +75,28 @@ const computed = {
   }),
 };
 
-const components = { "profile-summary": ProfileSummary };
-export default { components, computed, data };
+const data = function () {
+  return {
+    username: "this.user.username",
+    email: "user.email",
+    password: "******",
+    repeatedPassword: "",
+    age: 33,
+    height: 177,
+    weight: 21,
+  };
+};
+
+const components = {
+  "profile-summary": ProfileSummary,
+  "weight-chart": Weight,
+};
+
+export default {
+  components,
+  computed,
+  data,
+};
 </script>
 <style>
 .content .header {
