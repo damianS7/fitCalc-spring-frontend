4f1ce93cff2b091b693aed286a25e7b87b3c8f24
damianS7 <>
2021-07-20 20:41:34 +0200

    Reestructurado para mayor modularidad

---
+++ src/views/food/FoodIndex.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-col cols="12">
+    <b-row align-v="center">
+      <b-col cols="12">
+        <h1>Comidas</h1>
+      </b-col>
+    </b-row>
+    <b-row class="mb-2">
+      <b-col cols="12">
+        <b-form-input
+          v-model="searchFilter"
+          placeholder="Busca una comida"
+        ></b-form-input>
+      </b-col>
+    </b-row>
+    <b-row>
+      <food-list :foodList="foods"></food-list>
+    </b-row>
+  </b-col>
+</template>
+<script>
+import { mapGetters } from "vuex";
+import FoodList from "@/views/food/FoodList.vue";
+const components = { "food-list": FoodList };
+const data = function () {
+  return {
+    searchFilter: "",
+  };
+};
+const computed = {
+  ...mapGetters({ getFoods: "getFoods" }),
+  foods: function () {
+    return this.getFoods.filter((food) =>
+      food.name.toLowerCase().includes(this.searchFilter.toLowerCase())
+    );
+  },
+};
+
+export default { computed, components, data };
+</script>
+
+<style>
+</style>
--- src/views/food/FoodList.vue
+++ src/views/food/FoodList.vue
@@ -1,45 +1,17 @@
 <template>
   <b-col cols="12">
-    <b-row align-v="center">
-      <b-col cols="12">
-        <h1>Foods</h1>
-      </b-col>
-    </b-row>
-    <b-row class="mb-2">
-      <b-col cols="12">
-        <b-form-input
-          v-model="searchFilter"
-          placeholder="Busca una comida"
-        ></b-form-input>
-      </b-col>
-    </b-row>
     <food-item
-      v-for="food of foods"
+      v-for="food of foodList"
       v-bind:key="food.id"
       :food="food"
     ></food-item>
   </b-col>
 </template>
 <script>
-import { mapGetters } from "vuex";
 import FoodListItem from "@/views/food/FoodListItem.vue";
 const components = { "food-item": FoodListItem };
-const data = function () {
-  return {
-    searchFilter: "",
-  };
-};
-const computed = {
-  ...mapGetters({ getFoods: "getFoods" }),
-  foods: function () {
-    return this.getFoods.filter((food) =>
-      food.name.toLowerCase().includes(this.searchFilter.toLowerCase())
-    );
-  },
-};
-
-export default { computed, components, data };
+const props = ["foodList"];
+export default { components, props };
 </script>
-
 <style>
 </style>
