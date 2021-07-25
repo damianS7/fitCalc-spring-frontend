17f6f94d51cb6790a40087565ce1b907dbce358d
damianS7 <>
2021-07-25 03:01:27 +0200

    Modificacion de estilo visual

--- src/views/Weight.vue
+++ src/views/Weight.vue
@@ -1,52 +1,59 @@
 <template>
   <b-col cols="12">
-    <b-row class="mb-1">
+    <b-row class="mb-2">
       <b-col cols="12">
         <h1>Seguimiento</h1>
       </b-col>
     </b-row>
-    <b-row class="mb-1">
-      <b-col class="mb-3">
-        <weight-chart :chartData="chartData"> </weight-chart>
-      </b-col>
-    </b-row>
 
-    <b-row class="mb-1">
-      <b-col cols="12" sm="10" class="mb-2">
-        <b-form-select
-          id="inline-form-custom-select-pref"
-          v-model="selectedWeightDate"
-          :options="getWeights"
-        >
-          <template #first>
-            <b-form-select-option :value="null" disabled>
-              Selecciona la fecha del peso a borrar
-            </b-form-select-option>
-          </template>
-        </b-form-select>
-      </b-col>
-      <b-col class="mb-2">
-        <b-button @click="deleteSelectedWeight" class="w-100" variant="danger"
-          >-</b-button
-        >
-      </b-col>
-    </b-row>
+    <b-row class="mb-1 widget">
+      <b-col>
+        <b-row>
+          <b-col class="mb-3">
+            <weight-chart :chartData="chartData"> </weight-chart>
+          </b-col>
+        </b-row>
 
-    <b-row>
-      <b-col cols="12" sm="10" class="mb-2">
-        <b-form-input
-          id="weight"
-          v-model="add_weight"
-          type="number"
-          min="1"
-          max="400"
-          placeholder="Introduce tu peso de hoy"
-        ></b-form-input>
-      </b-col>
-      <b-col class="mb-2">
-        <b-button @click="addWeight()" class="w-100" variant="primary"
-          >+</b-button
-        >
+        <b-row class="mb-1">
+          <b-col cols="12" sm="10" class="mb-2">
+            <b-form-select
+              id="inline-form-custom-select-pref"
+              v-model="selectedWeightDate"
+              :options="getWeights"
+            >
+              <template #first>
+                <b-form-select-option :value="null" disabled>
+                  Selecciona la fecha del peso a borrar
+                </b-form-select-option>
+              </template>
+            </b-form-select>
+          </b-col>
+          <b-col class="mb-2">
+            <b-button
+              @click="deleteSelectedWeight"
+              class="w-100"
+              variant="danger"
+              >-</b-button
+            >
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="12" sm="10" class="mb-2">
+            <b-form-input
+              id="weight"
+              v-model="add_weight"
+              type="number"
+              min="1"
+              max="400"
+              placeholder="Introduce tu peso de hoy"
+            ></b-form-input>
+          </b-col>
+          <b-col class="mb-2">
+            <b-button @click="addWeight()" class="w-100" variant="primary"
+              >+</b-button
+            >
+          </b-col>
+        </b-row>
       </b-col>
     </b-row>
   </b-col>
