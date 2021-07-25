63a1f6aee0b5732512d70bbaa03b75f30eeb3982
damianS7 <>
2021-07-25 02:32:23 +0200

    Modificacion de ruta

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -7,7 +7,7 @@
         </router-link>
       </b-col>
       <b-col cols="4">
-        <router-link to="/meals-record">
+        <router-link to="/meals">
           <font-awesome-icon icon="book" />
         </router-link>
       </b-col>
